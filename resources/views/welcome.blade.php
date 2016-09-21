94801df34a88a945dfe4e60cb3901944a63710d4
choeungeol <ceg0416@naver.com>
2016-09-21 16:16:16 +0900

    second

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Idealize
                 </div>
 
                 <div class="links">
